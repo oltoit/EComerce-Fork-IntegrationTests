fd62d0182ad328ad02cf62d3f9701e442d37ae96
oltoit <Oliver.Tomaskovic@doubleslash.de>
2026-03-20 08:04:17 +0100

    chore: readded user2 to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 -- Users
 -- Normal User: username: user, password: user, role: USER
 insert into app_user (id, username, password, role) values (1, 'user1', '$2a$10$UcSltcvIoORWLKcCxo.4quqBWSoD0ZdC86caU..NSzNJdOdqgrKx2', 'USER');
+insert into app_user (id, username, password, role) values (2, 'user2', '$2a$10$egfNXcVS6VnfxpoJrqNIJO2za/OyfQbPU81YgqH8eqdKFN6F8fcs6', 'USER');
 -- Admin / Super User: username: admin, password: admin, role: ADMIN
 insert into app_user (id, username, password, role) values (3, 'admin', '$2a$10$1e2I81sXJW0gJDKyDZxhc.JXXIpK1Y0T4AKp0GwNs32370Wa2bDqK', 'ADMIN');
